8724cd43ed198805f37660541f72a09668cd5088
Ben Ledingham <benledingham1@gmail.com>
2024-01-07 15:33:01 -0800

    added difficulty scaling value to base spawners on

--- Managers/GameManager/DifficultyManager.cs
+++ Managers/GameManager/DifficultyManager.cs
@@ -3,13 +3,17 @@ using Godot;
 public partial class DifficultyManager: Node2D
 {
     public bool bossfight = false;
-    public float difficultyScale = 0;
-
+    public float difficultyScale;
     private Timer difficultyTimer;
 
     public override void _Ready()
     {
         this.difficultyTimer = GetNode<Timer>("DifficultyTimer");
         this.difficultyTimer.Start();
+    }
+
+    public override void _Process(double delta)
+    {
+        this.difficultyScale = 100/(float)this.difficultyTimer.TimeLeft;
     }
 }
