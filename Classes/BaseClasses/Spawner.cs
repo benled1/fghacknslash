a6bba7bdf61d03e56adda4c2a770e68fb6e2fef5
Ben Ledingham <benledingham1@gmail.com>
2024-01-07 22:22:54 -0800

    got the spawner to smart spawn at a random tile location

--- Classes/BaseClasses/Spawner.cs
+++ Classes/BaseClasses/Spawner.cs
@@ -1,13 +1,13 @@
 using Godot;
+using Godot.Collections;
+using System;
 
 
 
 public partial class Spawner: Node2D
 {
-
     public PackedScene spawnableEntity;
     public float spawnInterval;
 
     protected virtual void spawnEntity() {}
-
 }
--- Spawners/PigSpawner/PigSpawner.cs
+++ Spawners/PigSpawner/PigSpawner.cs
@@ -1,38 +1,56 @@
 using System;
 using Godot;
+using Godot.Collections;
 
 
 public partial class PigSpawner: Spawner
 {
     public float beginningSpawnDifficulty = 400;
-    
+
     private GameManager gameManager;
-    private Random random = new Random();
     private int spawnTickCount = 0;
+    private Array<Vector2I> possibleSpawnLocations;
+    private TileMap tileMap;
+    private Random random = new Random();
 
     public override void _Ready()
     {
+        base._Ready();
         this.gameManager = GetTree().CurrentScene.GetNode<GameManager>("GameManager");
         this.spawnableEntity = ResourceLoader.Load<PackedScene>("res://Entities/Enemies/Pig/Pig.tscn");
-    }
-    protected override void spawnEntity()
-    {
-        Pig spawnedPig = this.spawnableEntity.Instantiate<Pig>();
-        spawnedPig.Position = this.Position;
-        GetTree().CurrentScene.AddChild(spawnedPig);
-
+        this.tileMap = GetTree().CurrentScene.GetNode<TileMap>("TileMap");
+        this.possibleSpawnLocations = getSpawnLocations();
     }
 
     public override void _Process(double delta)
     {
         _updateSpawnInterval();
-        GD.Print(this.gameManager.difficultyManager.difficultyScale);
         this.spawnTickCount += 1;
         if (this.spawnTickCount >= this.spawnInterval)
         {
             spawnEntity();
             this.spawnTickCount = 0;
         }
+    }
+
+    protected override void spawnEntity()
+    {
+        Pig spawnedPig = this.spawnableEntity.Instantiate<Pig>();
+        spawnedPig.Position = this.possibleSpawnLocations[this.random.Next(0, possibleSpawnLocations.Count-1)];
+        GetTree().CurrentScene.AddChild(spawnedPig);
+    }
+
+    private Array<Vector2I> getSpawnLocations()
+    {
+        Array<Vector2I> possibleSpawnTileLocations = this.tileMap.GetUsedCellsById(layer: 0, sourceId: 0); // this is a Godot array
+        Vector2I tileSize = this.tileMap.TileSet.TileSize;
+
+        for (int i=0;i<possibleSpawnTileLocations.Count;i++)
+        {
+            Vector2I tempVector = new Vector2I(possibleSpawnTileLocations[i].X * tileSize.X, possibleSpawnTileLocations[i].Y * tileSize.Y);
+            possibleSpawnTileLocations[i] = tempVector;
+        }
+        return possibleSpawnTileLocations;
     }
 
     private void _updateSpawnInterval()
